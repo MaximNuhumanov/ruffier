65dcc2f1429f8b4246429bd1b75b35fe5ac9c956
Maxim <nugumanovmaxim@gmail.com>
2023-07-23 20:16:19 +0300

    Co-authored-by: Duzhbida <Duzhbida@users.noreply.github.com> Co-authored-by: vanyaivan770 <vanyaivan770@users.noreply.github.com> Co-authored-by: naziktazik13 <naziktazik13@users.noreply.github.com> Co-authored-by: scherbaka <scherbaka@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 from ruffier import test
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+class InstrSCR(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        
+class HeartCheck(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(InstrSCR(name = 'instr'))
+
+        return sm
+app = HeartCheck()
+app.run()
--- ruffier.py
+++ ruffier.py
@@ -1,3 +1,13 @@
+text_index = "Ваш індекс Руф'є: "
+text_workheart = "Працездатність серця: "
+list_rez = [
+    "Звернітся до лікаря, вам потрібний медичний огляд",
+    "Поганий функціональний стан",
+    "Середній функціональний стан",
+    "Гарний функціональний стан",
+    "Чудовий функціональний стан"]
+
+
 def ruffier_index(P1, P2, P3):
     ruffier_index = (4 * (P1 + P2 + P3) - 200) / 10
     return ruffier_index
@@ -31,7 +41,12 @@ def ruffier_level(ruffier_ind, max_ruffier_ind):
     return 4
     
 def test(P1,P2,P3,age):
+    if age < 7:
+        return "Неможливо розрахувати індекс Руф'є (для віку нижче 7 років недостатньо досліджень щоб робити висновки стосовно стану здоров'я)"
     ruffier_ind = ruffier_index(P1,P2,P3)
+    if ruffier_ind < 0:
+        return "Введений пульс занадто низький. Можливо ви помилились під час вимірювання, або вам варто звернутись до лікаря" 
     max_ruffier_ind = max_ruffier_index(age)
     level = ruffier_level(ruffier_ind, max_ruffier_ind)
-    
+    return text_index + str(ruffier_ind) + "\n" + text_workheart + list_rez[level]
+
