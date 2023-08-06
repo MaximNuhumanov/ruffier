8cdce9b524892831f7afae73443af7412e319ef8
Maxim <nugumanovmaxim@gmail.com>
2023-08-06 20:16:48 +0300

    Co-authored-by: Duzhbida <Duzhbida@users.noreply.github.com> Co-authored-by: vanyaivan770 <vanyaivan770@users.noreply.github.com> Co-authored-by: Andrianov_Roman <nd05@users.noreply.github.com> Co-authored-by: naziktazik13 <naziktazik13@users.noreply.github.com>

--- main.py
+++ main.py
@@ -9,56 +9,128 @@ from kivy.uix.textinput import TextInput
 
 name = ''
 age = 7
+P1 = 0
+P2 = 0
+P3 = 0
 
 class InstrSCR(Screen):
-    def __init__(self, **kwargs):
+    def __init__(self, mainLableText, lable1Text, lable2Text, **kwargs):
         super().__init__(**kwargs)
         
-        instr = Label(text = txt_instruction)
+        instr = Label(text = mainLableText)
         
         h1BoxLayout = BoxLayout(size_hint = (0.8, None), height = '30sp')
         h2BoxLayout = BoxLayout(size_hint = (0.8, None), height = '30sp')
+    
         
-        HL1 = Label(text = "Введіть ім'я: ")
+        HL1 = Label(text = lable1Text)
         self.in_name = TextInput(multiline = False)
         h1BoxLayout.add_widget(HL1)
         h1BoxLayout.add_widget(self.in_name)
         
-        HL2 = Label(text = "Введіть вік: ")
+        HL2 = Label(text = lable2Text)
         self.in_age = TextInput(text = '7', multiline = False)
         h2BoxLayout.add_widget(HL2)
         h2BoxLayout.add_widget(self.in_age)
 
         self.Button1 = Button(text = 'Почати', size_hint = (0.3, 0.2), pos_hint = {'center_x': 0.5})
-        self.Button1.on_press = self.next
+        if kwargs['name'] == 'instr':
+            self.Button1.on_press = self.next_start
+        else:
+            self.Button1.on_press = self.next_end
 
-        v1BoxLayout = BoxLayout(orientation = "vertical", padding = 8, spacing = 8 )
+        vBoxLayout = BoxLayout(orientation = "vertical", padding = 8, spacing = 8 )
 
-        v1BoxLayout.add_widget(instr)
-        v1BoxLayout.add_widget(h1BoxLayout)
-        v1BoxLayout.add_widget(h2BoxLayout)
-        v1BoxLayout.add_widget(self.Button1 )
+        vBoxLayout.add_widget(instr)
+        vBoxLayout.add_widget(h1BoxLayout)
+        vBoxLayout.add_widget(h2BoxLayout)
+        vBoxLayout.add_widget(self.Button1)
 
-        self.add_widget(v1BoxLayout)
 
-    def next(self):
+        self.add_widget(vBoxLayout)
+        
+    def next_end(self):
+        global P1, P2
+        try:
+            P1 = int(self.in_name.text)
+            P2 = int(self.in_age.text)
+            self.manager.current = ''
+        except:
+            self.in_name.text = "input Error"
+            self.in_age.text = "input Error"
+
+    def next_start(self):
         global name, age
         name = self.in_name.text
         if not name:
             name = "user"
         try:
             age = int(self.in_age.text)
-            #self.manager.current = ''
+            self.manager.current = 'pulsescr'
         except:
             self.in_age.text = "input Error"
+
+class PulseSCR(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        instr = Label(text = txt_test1)
+        
+        hBoxLayout = BoxLayout(size_hint = (0.8, None), height = '30sp')
         
 
+        H3 = Label(text = "Введіть результат:")
+        self.in_pulse = TextInput(multiline = False)
+        hBoxLayout.add_widget(H3)
+        hBoxLayout.add_widget(self.in_pulse)
+
+        self.Button2 = Button(text = 'Продовжити', size_hint = (0.3, 0.2), pos_hint = {'center_x': 0.5})
+        self.Button2.on_press = self.next
+
+        vBoxLayout = BoxLayout(orientation = "vertical", padding = 8, spacing = 8 ) 
+
+        vBoxLayout.add_widget(instr)
+        vBoxLayout.add_widget(hBoxLayout)
+        vBoxLayout.add_widget(self.Button2) 
+
+        self.add_widget(vBoxLayout)
+
+    def next(self):
+        global P1
+        try:
+            P1 = int(self.in_pulse.text)
+            self.manager.current = 'situps'
+        except:
+            self.in_pulse.text = "input Error"
         
+class SitUpsSCR(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         
+        instr = Label(text = txt_test2)
+
+        self.Button = Button(text = 'Продовжити', size_hint = (0.3, 0.2), pos_hint = {'center_x': 0.5})
+        self.Button.on_press = self.next
+
+        vBoxLayout = BoxLayout(orientation = "vertical", padding = 8, spacing = 8 ) 
+
+        vBoxLayout.add_widget(instr)
+        vBoxLayout.add_widget(self.Button)
+
+        self.add_widget(vBoxLayout)
+
+    def next(self):
+        self.manager.current = 'pulsescr2'
+        
+    
+
 class HeartCheck(App):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(InstrSCR(name = 'instr'))
+        sm.add_widget(InstrSCR(txt_instruction, "Введіть ім'я:", "Введіть вік:", name = 'instr'))
+        sm.add_widget(PulseSCR(name = 'pulsescr'))
+        sm.add_widget(SitUpsSCR(name = 'situps')) 
+        sm.add_widget(InstrSCR(txt_test3, 'Результат:', 'Результат після відпочинку:', name = 'pulsescr2'))
 
         return sm
 app = HeartCheck()
---
+++ text.py
@@ -0,0 +1,8 @@
+def func(**kwargs):
+    print(kwargs)
+
+func(a = 7, b = 8, d = 12, c = 45)
+
+def func1(*args):                                 
+    print(args)
+func1(7, 8, 12, 45)
