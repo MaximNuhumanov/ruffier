b8b8f07f319831cfe88d723e097e6c46582e2341
Maxim <nugumanovmaxim@gmail.com>
2023-08-13 19:52:53 +0300

    commit

---
+++ Seconds.py
@@ -0,0 +1,26 @@
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.properties import BooleanProperty
+
+
+
+class Second(Label):
+    
+    finished = BooleanProperty(False)
+    
+    def __init__(self, total, **kwargs):
+        self.current = 0
+        self.total = total
+        self.finished = False
+        my_text = 'Пройшло секунд' + str(self.current)
+        super().__init__(text = my_text, **kwargs)
+
+    def start(self):
+        Clock.schedule_interval(self.change, 1)
+        
+    def change(self, dt):
+        self.current += 1
+        self.text = 'Пройшло секунд' + str(self.current)
+        if self.current >= self.total:
+            self.finished = True
+            return False
